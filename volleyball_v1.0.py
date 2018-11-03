e908afc27bee06685553965f15bb1be3aec7c18f
yeseul-heo <41201416+yeseul-heo@users.noreply.github.com>
2018-11-03 14:56:50 -0400

    Update volleyball_v1.0.py

---
+++ volleyball_v1.0.py
@@ -0,0 +1,27 @@
+import csv
+import os
+
+class Application(Frame):
+    #Initial Settings
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.init_Buttons()
+        root.title("GT Volleyball Analytics")
+
+    #Place Initial Buttons
+    def init_Buttons(self):
+        #Three buttons for year, game, and player
+        self.toptext = Label(text="Select Category:").place(relx=0.5, rely=0.2, anchor=CENTER)
+        self.yearbutton = Button(text="Year").place(relx=0.5, rely=0.4, anchor=CENTER)
+        self.gamebutton = Button(text="Game").place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.playerbutton = Button(text="Player").place(relx=0.5, rely=0.6, anchor=CENTER)
+
+    
+
+#Final settings to keep window open
+root = Tk()
+root.geometry("1000x400")
+app = Application(master=root)
+app.mainloop()
+root.destroy()
